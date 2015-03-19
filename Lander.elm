6b5d11a91c870488e48e6060387cab3e6609672c
Laurent Bossavit <laurent.git@bossavit.com>
2015-03-19 23:28:08 +0100

    Draw a ship

--- Lander.elm
+++ Lander.elm
@@ -8,23 +8,35 @@ import Window
 
 -- Model
 
-start (w,h) = {w = w, h = h}
+start viewport = case viewport of
+    Viewport (w,h) -> {
+        view = {w = w, h = h},
+        ship = {x = 0, y = 0, vx =0, vy = 0}
+    }
 
 -- Update
 
-update (w,h) world = 
-    let wh = Debug.watch "dims" (w,h)
-    in {world | w<-w, h<-h}
+type Update = Viewport (Int, Int)
+
+update s world = case s of
+    Viewport dims -> updateViewport dims world
+
+updateViewport (w,h) world =
+    let wv = world.view
+        v' = { wv | w <- 0, h <- 0}
+    in {world | view <- v'}
 
 -- Display
 
 display world =
-    let (w',h') = (toFloat world.w, toFloat world.h)
-    in collage world.w world.h [filled black (rect w' h')]
+    let (w',h') = (toFloat world.view.w, toFloat world.view.h)
+    in collage world.view.w world.view.h [
+        filled black (rect w' h'),
+        rotate (degrees 90) <| outlined (solid white) (ngon 3 30)]
 
 -- Signals
 
-inputs = Window.dimensions
+inputs = Signal.map Viewport Window.dimensions
 
 main =
     let states = Signal.Extra.foldp' update start inputs
