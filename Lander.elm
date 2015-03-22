a5ba7e131341dcfeae184012722d1183203d1227
Laurent Bossavit <laurent.git@bossavit.com>
2015-03-22 12:18:50 +0100

    Keys control steering and thrust

--- Lander.elm
+++ Lander.elm
@@ -2,10 +2,12 @@ import Signal
 import Signal.Extra
 import Graphics.Element (..)
 import Graphics.Collage (..)
+import Transform2D (..)
 import Color (..)
 import Time (..)
 import Debug
 import Window
+import Keyboard
 -- App imports
 import Generic (..)
 
@@ -14,17 +16,18 @@ import Generic (..)
 start viewport = case viewport of
     Viewport (w,h) -> {
         view = {w = w, h = h},
-        ship = {x = 0, y = 0, vx =0, vy = 0.1},
+        ship = {x = 0, y = 0, vx =0, vy = 0.1, r=0},
         planet = {x = -150, y = 0, r = 70}
     }
 
 -- Update
 
-type Update = Viewport (Int, Int) | Tick Float
+type Update = Viewport (Int, Int) | Tick Float | Move {x:Int,y:Int}
 
 update s world = case s of
     Viewport dims -> updateViewport dims world
     Tick dt -> let n = 8 in iterate (updateTick (dt/n)) n world
+    Move arrows -> updateMove arrows world
 
 updateViewport (w,h) world =
     let wv = world.view
@@ -47,21 +50,32 @@ updateTick dt world =
         }
     in {world | ship <- s'}
 
+updateMove arrows world = 
+    let s = world.ship
+        s' = {s | r <- s.r-(toFloat arrows.x)*5,
+                  vx <- s.vx - (toFloat arrows.y) * sin (degrees s.r) * 0.001,
+                  vy <- s.vy + (toFloat arrows.y) * cos (degrees s.r) * 0.001}
+    in {world | ship <- s'}
+
 -- Display
 
 display world =
     let (w',h') = (toFloat world.view.w, toFloat world.view.h)
     in collage world.view.w world.view.h [
         filled black (rect w' h'),
-        Debug.trace "ship" (move (world.ship.x, world.ship.y) <| rotate (degrees 90) <| outlined (solid white) (ngon 3 30)),
+        Debug.trace "ship" (
+            move (world.ship.x, world.ship.y) <| rotate (degrees world.ship.r) <|
+                group [rotate (degrees -30) (outlined (solid white) (ngon 3 25)), outlined (solid white) (rect 3 25)]
+            ),
         move (world.planet.x, world.planet.y) <| outlined (solid white) (circle world.planet.r)]
 
 -- Signals
 
 window = Signal.map Viewport Window.dimensions
 ticks = Signal.map Tick (fps 30)
+keys = Signal.sampleOn (fps 30) (Signal.map Move Keyboard.arrows)
 
-inputs = Signal.mergeMany [window, ticks]
+inputs = Signal.mergeMany [window, ticks, keys]
 
 main =
     let states = Signal.Extra.foldp' update start inputs
