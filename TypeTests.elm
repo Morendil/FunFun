0ac1ef037440cda3281b16d858995e890b366cff
Laurent Bossavit <laurent.git@bossavit.com>
2015-03-28 22:11:49 +0100

    Some variants on the record subtyping problem

--- TypeTests.elm
+++ TypeTests.elm
@@ -1,13 +1,9 @@
-import Graphics.Element (..)
 import Text (..)
 
 rec1 = {x="foo",y=11}
 rec2 = {x="bar",z=12}
 
-extract : {a|x:String} -> String
-extract record = (foo record).x
+extract : {a|x:String} -> {a|x:String}
+extract r = {r | x <- r.x++"!"}
 
-foo : {a|x:String} -> {a|x:String}
-foo r = {r | x <- r.x++"!"}
-
-main = plainText <| extract rec1 ++ extract rec2
+main = plainText <| (extract rec1).x ++ (extract rec2).x 
--- TypeTests2.elm
+++ TypeTests2.elm
@@ -1,7 +1,4 @@
-import Graphics.Element (..)
 import Text (..)
-import List
-import String
 
 type Record = This {x:String,y:Float} | That {x:String,z:Float}
 
@@ -11,8 +8,8 @@ rec2 = That {x="bar",z=12}
 extract : Record -> {a|x:String}
 extract wrapped =
     case wrapped of
-        This record -> record
-        That record -> record
+        This r -> {x=r.x}
+        That r -> {x=r.x}
 
 main = plainText <| (extract rec1).x ++ (extract rec2).x
     -- String.concat (List.map (.x) (List.map extract [rec1,rec2]))
