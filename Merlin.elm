bd46b8303b111725d969fb4277cd6f2a73fc5e57
Laurent Bossavit <laurent.git@bossavit.com>
2015-04-04 15:15:23 +0200

    Fill window

--- Merlin.elm
+++ Merlin.elm
@@ -1,10 +1,31 @@
 import Graphics.Collage (..)
 import Color (..)
 
+import Signal
+import Signal.Extra
+import Window
+
 -- Model
+
+start (w,h) =
+    {view={w=w,h=h}}
+
 -- Update
+
+update (w,h) world =
+    let view = world.view
+        view' = {view | w<-w,h<-h}
+    in {world | view <- view'}
+
 -- Display
+
+display world =
+    collage world.view.w world.view.h <| [filled black <| rect (toFloat world.view.w) (toFloat world.view.h)]
+
 -- Signals
 
+inputs = Window.dimensions
+
 main =
-    collage 640 480 <| [filled black <| rect 640 480]
+    let states = Signal.Extra.foldp' update start inputs
+    in Signal.map display states
