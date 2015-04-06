8d018c224db632f764e97194a19cfb78507edcba
Laurent Bossavit <laurent.git@bossavit.com>
2015-04-06 12:18:46 +0200

    Display square based on state

--- Merlin.elm
+++ Merlin.elm
@@ -4,14 +4,22 @@ import Graphics.Collage (..)
 import Color (..)
 
 import List
+
+import Mouse
 import Signal
 import Signal.Extra
 import Window
 
 -- Model
 
-start (w,h) =
-    {view={w=w,h=h}}
+size = 5
+
+start u =
+    case u of
+        Viewport (w,h) -> {
+            view={w=w,h=h},
+            states=List.map (always False) [1..size^2]
+        }
 
 gridSize (w,h) = (min w h)*2/3
 divide n side =
@@ -20,27 +28,43 @@ divide n side =
 
 -- Update
 
-update (w,h) world =
+type Update = Viewport (Int, Int) | Click (Int,Int)
+
+update u world =
+    case u of
+        Viewport vp -> updateViewport vp world
+        Click coords -> updateClick coords world
+
+updateClick (x,y) world =
+    world
+
+updateViewport (w,h) world =
     let view = world.view
         view' = {view | w<-w,h<-h}
     in {world | view <- view'}
 
 -- Display
 
+displaySquare row col world =
+    let side = gridSize (toFloat world.view.w,toFloat world.view.h)
+        small = divide size side
+        frnd = toFloat << round
+        state = List.head (List.drop ((((floor row)-1)*size)+(floor col)-1) world.states)
+        style = if state then filled green else outlined (solid white)
+    in move ((small+2)*(frnd (size/2-row)),(small+2)*(frnd (size/2-col))) <| style (rect small small)
+
 display world =
-    let n = 5
-        backdrop = filled black <| rect (toFloat world.view.w) (toFloat world.view.h)
+    let backdrop = filled black <| rect (toFloat world.view.w) (toFloat world.view.h)
         side = gridSize (toFloat world.view.w,toFloat world.view.h)
         grid = outlined (solid white) (rect side side)
-        small = divide n side
-        frnd = toFloat << round
-        square row col = move ((small+2)*(frnd (n/2-row)),(small+2)*(frnd (n/2-col))) <| filled green <| rect small small
-        squares = List.concatMap (\row -> List.map (\col -> square row col) [1..n]) [1..n]
+        squares = List.concatMap (\row -> List.map (\col -> displaySquare row col world) [1..size]) [1..size]
     in collage world.view.w world.view.h <| List.concat [[backdrop,grid],squares]
 
 -- Signals
 
-inputs = Window.dimensions
+mouseClicks = Signal.map Click (Signal.sampleOn Mouse.clicks Mouse.position)
+dimensions = Signal.map Viewport (Window.dimensions)
+inputs = Signal.mergeMany [dimensions,mouseClicks]
 
 main =
     let states = Signal.Extra.foldp' update start inputs
