5e1a3ac6cddc3480ae0880ca0c442bf72b625eba
Laurent Bossavit <laurent.git@bossavit.com>
2015-04-09 22:47:06 +0200

    Draw a grid of squares, centered

--- MatchThree.elm
+++ MatchThree.elm
@@ -14,6 +14,8 @@ import Debug
 
 -- Model
 
+size = 15
+
 start u =
     case u of
         Viewport (w,h) -> {view={w=w,h=h}}
@@ -26,10 +28,25 @@ update u world = world
 
 -- Display
 
+displaySquare row col world =
+    let icon = collage 20 20 <| [filled blue (rect 20 20)]
+    in clickable (Signal.send locations (row,col)) icon
+
+rowOfSquares world row =
+    let squares = map (\col -> displaySquare row col world) [1..size]
+        between = spacer 2 2
+    in flow right (intersperse between squares)
+
+makeSquares world =
+    let rows = map (rowOfSquares world) [1..size]
+        between = spacer 2 2
+    in flow down (intersperse between rows)
+
 display world =
     let (w',h') = (toFloat world.view.w, toFloat world.view.h)
-        backdrop = filled black <| rect w' h'
-    in collage world.view.w world.view.h [backdrop]
+        backdrop = collage world.view.w world.view.h [filled black <| rect w' h']
+        squares = makeSquares world
+    in layers [backdrop, container world.view.w world.view.h middle squares]
 
 -- Signals
 
