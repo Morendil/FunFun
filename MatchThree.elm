a1b3c93cdc85b6aeabac19f38795b64b63f943af
Laurent Bossavit <laurent.git@bossavit.com>
2015-04-12 16:13:23 +0200

    Separate clickables from drawing, so that motion is possible; empty squares by clicking

--- MatchThree.elm
+++ MatchThree.elm
@@ -1,5 +1,8 @@
 module MatchThree where
 
+import AnimationFrame (..)
+import Time (..)
+
 import Graphics.Input (..)
 import Graphics.Element (..)
 import Graphics.Collage (..)
@@ -25,27 +28,40 @@ start u =
         in {
             view={w=w,h=h},
             states = states,
-            seed = seed
+            seed = seed,
+            time = 0
         }
 
 -- Update
 
-type Update = Viewport (Int, Int) | Click (Int,Int)
+type Update = Viewport (Int, Int) | Click (Int,Int) | Frame Time
 
-update u world = world
+update u world =
+   case u of
+        Click (row,col) ->
+            let index = (row-1)*size + (col-1)
+                states' = take index world.states ++ (0 :: drop (index+1) world.states)
+            in {world | states <- states'}
+        Frame dt -> {world | time <- world.time + dt}
+        _ -> world
 
 -- Display
 
-displaySquare row col world =
+displaySquare world row col  =
+    clickable (Signal.send locations (row,col)) (collage 20 20 [])
+
+displayIcon world row col =
     let index = (row-1)*size + (col-1)
+        x = toFloat (10+(col-1)*22)-((size*22-2)/2)
+        y = ((size*22-2)/2) - (toFloat (10+(row-1)*22))
         state = head (drop index world.states)
         color state = head (drop (state-1) [green,blue,red,white])
         shape state = head (drop (state-1) [circle 10,ngon 3 10,rect 20 20,ngon 5 10])
-        icon = collage 20 20 <| [filled (color state) (shape state)]
-    in clickable (Signal.send locations (row,col)) icon
+    in if state == 0 then toForm empty
+        else move (x,y) <| filled (color state) (shape state)
 
 rowOfSquares world row =
-    let squares = map (\col -> displaySquare row col world) [1..size]
+    let squares = map (displaySquare world row) [1..size]
         between = spacer 2 2
     in flow right (intersperse between squares)
 
@@ -54,18 +70,25 @@ makeSquares world =
         between = spacer 2 2
     in flow down (intersperse between rows)
 
+makeIcons world =
+    let rowOfIcons world row = map (displayIcon world row) [1..size]
+    in  collage (size*22-2) (size*22-2) (concatMap (rowOfIcons world) [1..size])
+
 display world =
     let (w',h') = (toFloat world.view.w, toFloat world.view.h)
         backdrop = collage world.view.w world.view.h [filled black <| rect w' h']
         squares = makeSquares world
-    in layers [backdrop, container world.view.w world.view.h middle squares]
+        icons = makeIcons world
+    in layers [backdrop, container world.view.w world.view.h middle icons, container world.view.w world.view.h middle squares]
 
 -- Signals
 
 locations = Signal.channel (0,0)
+
+frames = Signal.map Frame frame
 buttons = Signal.map Click (Signal.subscribe locations)
 dimensions = Signal.map Viewport (Window.dimensions)
-inputs = Signal.mergeMany [dimensions,buttons]
+inputs = Signal.mergeMany [dimensions,buttons,frames]
 
 main =
     let states = Signal.Extra.foldp' update start inputs
