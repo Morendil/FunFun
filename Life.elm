583b3a70a15ce7cb17277d734fbe81563af06c6c
Laurent Bossavit <laurent.git@bossavit.com>
2015-04-26 14:54:54 +0200

    Display a glider

--- Life.elm
+++ Life.elm
@@ -1,6 +1,5 @@
-module Life where
+module Life (main) where
 
-import Graphics.Input (..)
 import Graphics.Element (..)
 import Graphics.Collage (..)
 import List (..)
@@ -16,27 +15,36 @@ import Debug
 
 start u =
     case u of
-        Viewport (w,h) -> {view={w=w,h=h}}
+        Viewport (w,h) -> {
+            view={w=w,h=h},
+            live=[(0,1),(1,0),(-1,-1),(0,-1),(1,-1)]
+        }
 
 -- Update
 
 type Update = Viewport (Int, Int) | Click (Int,Int)
 
-update u world = world
+update u world =
+    case u of 
+        Viewport (w,h) ->
+            let view' = {w=w,h=h}
+            in { world | view <- view'}
 
 -- Display
+
+displayCell (row,col) =
+    move (row*11,col*11) <| filled white <| rect 10 10
 
 display world =
     let (w',h') = (toFloat world.view.w, toFloat world.view.h)
         backdrop = filled black <| rect w' h'
-    in collage world.view.w world.view.h [backdrop]
+        cells = map displayCell world.live
+    in collage world.view.w world.view.h (backdrop :: cells)
 
 -- Signals
 
-locations = Signal.channel (0,0)
-buttons = Signal.map Click (Signal.subscribe locations)
 dimensions = Signal.map Viewport (Window.dimensions)
-inputs = Signal.mergeMany [dimensions,buttons]
+inputs = Signal.mergeMany [dimensions]
 
 main =
     let states = Signal.Extra.foldp' update start inputs
