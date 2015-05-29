6a125763e66a1264e4118d6b2ffacd9a69353160
Laurent Bossavit <laurent.git@bossavit.com>
2015-05-29 15:41:00 +0200

    Money example

---
+++ MoneyChecks.elm
@@ -0,0 +1,44 @@
+import Check exposing (..)
+import Shrink exposing (noShrink)
+import Check.Investigator exposing (..)
+import Check.Runner.Browser exposing (..)
+import Random exposing (..)
+import Random.Int exposing (..)
+import Random.Float exposing (..)
+
+type Currency = EUR | GBP | USD
+type alias Money = {currency:Currency, amount:Float}
+
+selectCurrency selector =
+    case selector of
+            1 -> EUR
+            2 -> GBP
+            3 -> USD
+
+moneyGen seed = 
+    let currencyGenerator = Random.int 1 3
+        amountGenerator = anyFloat
+        (currencySelector, seedA) = generate currencyGenerator seed
+        (actualAmout, seedB) = generate amountGenerator seedA
+        actualCurrency = selectCurrency currencySelector
+    in ({currency = actualCurrency, amount=actualAmout}, seedB)
+
+money = investigator (customGenerator moneyGen) noShrink
+
+add money1 money2 = Nothing
+
+moneyClaim =
+    claim
+        "Adding two different currencies yields an error"
+      `that`
+        (\(money1,money2) -> add money1 money2)
+      `is`
+        (always Nothing)
+      `for`
+        tuple (money, money)
+
+moneySuite = suite "Money suite" [moneyClaim]
+
+result = quickCheck moneySuite
+
+main = display result
