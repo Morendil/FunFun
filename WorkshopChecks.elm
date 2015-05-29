884081a2db88ae1e43ca65a582c010db22706422
Laurent Bossavit <laurent.git@bossavit.com>
2015-05-29 15:03:32 +0200

    Examples: integer squaring

--- WorkshopChecks.elm
+++ WorkshopChecks.elm
@@ -2,17 +2,20 @@ import Check exposing (..)
 import Check.Investigator exposing (..)
 import Check.Runner.Browser exposing (..)
 
+square : Int -> Int
 square x = x * x
+sqrti : Int -> Int
+sqrti = floor << sqrt << toFloat
 
 claim_squaring_root_yields_original =
   claim
     "Squaring the root of a number yields the original number"
   `that`
-    (\x -> square (sqrt x))
+    (\x -> sqrti (square x))
   `is`
     (identity)
   `for`
-    float
+    int
 
 suite_math =
   suite "Math suite"
