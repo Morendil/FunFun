f21e46e6e79e83db8d5beda4889699cbc3ab7ff3
Laurent Bossavit <laurent.git@bossavit.com>
2015-06-20 15:35:25 +0200

    Move cell around

--- Agar.elm
+++ Agar.elm
@@ -3,10 +3,13 @@ module Agar where
 import AnimationFrame exposing (..)
 import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
+import Generic exposing (..)
 import List exposing (..)
 import Color exposing (..)
 
+import Debug
 import Window
+import Mouse
 import Keyboard
 import Signal.Extra
 
@@ -15,17 +18,31 @@ import Signal.Extra
 start u =
     case u of
         Viewport (w,h) -> {
-                view={w=w,h=h}
+                view={w=w,h=h},
+                pos={x=0,y=0}
         }
 
 -- Update
 
-type Update = Viewport (Int, Int) | Frame Float
+type Update = Viewport (Int, Int) | Frame Float (Int, Int)
 
 update u world = 
     case u of
         Viewport vp -> updateViewport vp world
-        _ -> world
+        Frame dt cursor ->
+            let fps = Debug.watch "fps" <| floor (1000/dt)
+            in Debug.watch "world" <| glide world dt cursor
+
+glide world dt cursor =
+    let delta which base =
+            let val = which <| addPair (-world.view.w//2,-world.view.h//2) cursor
+            in base +  if | val <= -5 && val > -50 -> -1
+                          | val >= 5 && val < 50 -> 1
+                          | val <= -50 -> -2
+                          | val >= 50 -> 2
+                          | otherwise -> 0
+        pos' = {x=delta fst world.pos.x,y=-(delta snd -world.pos.y)}
+    in {world | pos <- pos'}
 
 updateViewport (w,h) world =
     let view = world.view
@@ -38,14 +55,17 @@ display world =
     let spacing = 40
         radius = 20
         player = collage world.view.w world.view.h [filled black <| circle (radius+2), filled red <| circle radius]
-        grid = collage world.view.w world.view.h <|
-                map (\k -> traced (dotted gray) <| segment (k*spacing,-1000) (k*spacing,1000)) [-500..500] ++
-                map (\k -> traced (dotted gray) <| segment (-1000,k*spacing) (1000,k*spacing)) [-500..500]
+        count = toFloat <| 2 * ((max world.view.w world.view.h) // (spacing*2))
+        offset coord = -(toFloat (floor coord % spacing))
+        lines = group <|
+                    map (\k -> traced (dotted gray) <| segment (k*spacing,-1000) (k*spacing,1000)) [-count/2..1+count/2] ++
+                    map (\k -> traced (dotted gray) <| segment (-1000,k*spacing) (1000,k*spacing)) [-count/2..1+count/2]
+        grid = collage world.view.w world.view.h [ move (offset world.pos.x,offset world.pos.y) lines]
     in layers [grid,player]
 
 -- Signals
 
-frames = Signal.map Frame frame
+frames = Signal.map2 Frame frame Mouse.position
 dimensions = Signal.map Viewport (Window.dimensions)
 
 inputs = Signal.mergeMany [dimensions,frames]
