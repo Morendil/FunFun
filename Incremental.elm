cbb43ff9e389793fa5851c8437dbe1d33d5de735
Laurent Bossavit <laurent.git@bossavit.com>
2015-07-26 20:53:00 +0200

    A rather gloomy way to continue.

--- Incremental.elm
+++ Incremental.elm
@@ -1,3 +1,22 @@
-import Html exposing (..)
+import Html exposing (text)
+import Time exposing (fps)
+import Signal
 
-main = text "All you can do is wait."
+import Array
+import Debug
+
+time = fps 0.2
+
+states = Array.fromList ["All you can do is wait.", "You die."]
+
+update state dt =
+    state + dt
+
+display state =
+    let which = min (floor state//100) ((Array.length states)-1)
+        (Just value) = Array.get which states
+    in text value
+
+main =
+    let states = Signal.foldp update 0 time
+    in Signal.map display states
