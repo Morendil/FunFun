94924d863547de16120d3d3fcbca1f94adbf1272
Laurent Bossavit <laurent.git@bossavit.com>
2015-07-28 13:27:25 +0200

    ADR style progress bar

--- Incremental.elm
+++ Incremental.elm
@@ -1,14 +1,16 @@
 module Incremental where
 
-import Html exposing (text)
+import Html exposing (text, div)
+import Html.Attributes exposing (class, style)
 import Time exposing (fps)
 import Signal
 
+import String
 import Array
 import Debug
 
 nodes = Array.fromList [
-        ("All you can do is wait.",1000),
+        ("All you can do is wait.",3000),
         ("You die.",0)
     ]
 
@@ -21,12 +23,18 @@ update dt world =
         next = world.node + 1
         limit = (Array.length nodes)-1
         node' = if time' > delay then max next limit else world.node
-    in {world | time <- world.time + dt, node <- node'}
+        time'' = if node' == world.node then time' else 0
+    in {world | time <- time'', node <- node'}
 
 display world =
     let (Just value) = Array.get world.node nodes
-    in text (fst value)
+        wait = snd value
+        percent = if wait > 0 then (100 * (1 - (world.time / wait))) else 0
+    in div [class "button"] [
+        text (fst value),
+        div [class "progress", style [("width",String.concat [toString percent, "%"])]] []
+    ]
 
 main =
-    let states = Signal.foldp update start (fps 10)
+    let states = Signal.foldp update start (fps 30)
     in Signal.map display states
