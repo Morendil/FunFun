a674729593a529f3429c3c720c31748df9ae3790
Laurent Bossavit <laurent.git@bossavit.com>
2015-09-22 23:21:15 +0200

    Draw the starting point

--- Zero.elm
+++ Zero.elm
@@ -1,30 +1,48 @@
 module Zero where
 
 import Graphics.Collage exposing (..)
+import Color exposing (..)
 
 import Signal
+import Window
+import Signal.Extra
+
 import Time exposing (fps)
 
 
 -- Model
 
-start = {}
+start u =
+    case u of
+        Viewport (w,h) -> {width=w,height=h}
 
 -- Update
 
-update u =
-  identity
+update u world =
+  case u of
+    Viewport (w,h) -> {world | width <- w, height <- h}
+    _ -> world
 
 -- Display
 
-display _ =
-    collage 0 0 []
+display world =
+    let radius = toFloat world.height/3.4
+        gray = (rgb 204 204 204)
+    in collage world.width world.height [
+        filled black <| circle radius,
+        filled gray <| circle (radius-14),
+        filled white <| rect (2+radius*2) 14,
+        move (0,14) <| filled black <| rect (radius*2-2) 14,
+        move (0,-14) <| filled black <| rect (radius*2-2) 14
+    ]
 
 -- Signals
 
-type Update = Frame Float
+type Update = Frame Float | Viewport (Int,Int)
 
 main =
     let frames = Signal.map Frame (fps 30)
-        states = Signal.foldp update start frames
+        dimensions = Signal.map Viewport (Window.dimensions)
+        signals = Signal.mergeMany [dimensions,frames]
+        states = Signal.Extra.foldp' update start signals
     in Signal.map display states
