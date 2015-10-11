ad00c59080521bae8da1eb463d580150dd08454f
Laurent Bossavit <laurent.git@bossavit.com>
2015-10-11 21:40:28 +0200

    Transition fill color

--- Zero.elm
+++ Zero.elm
@@ -20,13 +20,13 @@ start u =
     case u of
         Viewport (w,h) -> {width=w,height=h,time=0,items=[
             Background {color=Transition white black 5000},
-            HalfCircle {color=Transition black white 5000, angle=Still 0, pos=Transition (0,0) (0,-70) 5000},
-            HalfCircle {color=Transition blue white 5000, angle=Still 180, pos=Still (0,70)}]
+            HalfCircle {color=Transition black white 5000, fill=Transition gray black 5000, angle=Still 0, pos=Transition (0,0) (0,-70) 5000},
+            HalfCircle {color=Transition black white 5000, fill=Transition gray black 5000, angle=Still 180, pos=Still (0,70)}]
         }
 
 type Animated a = Still a | Transition a a Float
 type Item = Background {color:Animated Color} |
-            HalfCircle {color:Animated Color,angle:Animated Float,pos: Animated (Float,Float)} |
+            HalfCircle {color:Animated Color,fill:Animated Color,angle:Animated Float,pos: Animated (Float,Float)} |
             Star
 
 -- Update
@@ -67,14 +67,14 @@ display world =
     let radius = toFloat world.height/3.4
         gap = radius / 14
         gray = (rgb 204 204 204)
-        fill = ease Easing.linear Easing.color gray black 5000 world.time
         place x = collage world.width world.height [x]
         active x = clickable (Signal.message clicks.address x)
         animateColor = animate Easing.color world.time
         draw item = case item of
             Background {color} -> place <| filled (animateColor color) <| rect (toFloat world.width) (toFloat world.height)
-            HalfCircle {color,angle,pos} -> active South <|
-                place <| move (vecTimes (animate (Easing.pair Easing.float) world.time pos) (21/100)) <| rotate (degrees <| animate Easing.float world.time angle) <| north (animateColor color) fill radius
+            HalfCircle {color,fill,angle,pos} -> active South <|
+                place <| move (vecTimes (animate (Easing.pair Easing.float) world.time pos) (21/100)) <|
+                rotate (degrees <| animate Easing.float world.time angle) <| north (animateColor color) (animateColor fill) radius
             Star -> place <| move (gap*5,-gap*5) <| star world.time (radius/15)
     in layers <| List.map draw world.items
 
